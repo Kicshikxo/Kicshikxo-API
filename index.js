67924c0363f77525094728983940fc1f679d573c
Kicshikxo <supermegavalera9000@gmail.com>
2022-01-23 11:11:44 +0300

    added index file

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const http = require('http')
+
+const moobotCommands = require('./Moobot-commands/index')
+const timetable = require('./Timetable/index')
+const duties = require('./Duties/index')
+
+const app = express()
+app.use(cors())
+
+const server = http.createServer(app)
+
+app.use('/moobot-commands', moobotCommands)
+app.use('/timetable', timetable)
+duties.lister(server)
+
+module.exports = server
+
+if (require.main === module) {
+    const PORT = process.env.PORT || 3000
+    server.listen(PORT, () => {
+        console.log(`Server started on port: ${PORT}`)
+    })
+}
