e4b3ac3ed2e0c8bbe0d2647f2d745e940cfab714
Kicshikxo <supermegavalera9000@gmail.com>
2022-10-10 02:24:47 +0300

    Add lessonDto extensions

--- src/v2/timetable/dto/lesson.dto.ts
+++ src/v2/timetable/dto/lesson.dto.ts
@@ -13,3 +13,19 @@ export class lessonDto {
   @ApiProperty({ example: '42', description: 'Кабинет урока' })
   cabinet: string;
 }
+
+export class lessonWithDateDto extends lessonDto {
+  @ApiProperty({
+    example: '2022-09-14',
+    description: 'Дата в формате ISO-8601',
+  })
+  date: string;
+}
+
+export class lessonWithDateAndWeekIdDto extends lessonWithDateDto {
+  @ApiProperty({
+    example: '202237',
+    description: 'Идентификатор недели в формате iyyyiw',
+  })
+  weekId: string;
+}
