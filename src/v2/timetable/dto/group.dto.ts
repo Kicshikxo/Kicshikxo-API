98e562dddb97c4abc7a82b1aad6cceab3d899da3
Kicshikxo <supermegavalera9000@gmail.com>
2022-10-14 00:55:18 +0300

    Add group property to group dto

--- src/v2/timetable/dto/group.dto.ts
+++ src/v2/timetable/dto/group.dto.ts
@@ -7,6 +7,9 @@ export class groupDto {
   })
   id: string;
 
+  @ApiProperty({ example: 'ПКС-4.2', description: 'Название группы' })
+  group: string;
+
   @ApiProperty({ example: '2022-2023', description: 'Учебный год группы' })
   academicYear: string;
 }
