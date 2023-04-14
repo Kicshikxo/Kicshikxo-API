90829f5670763030ebe2266ea6096d02a618311e
Kicshikxo <supermegavalera9000@gmail.com>
2023-04-15 00:50:19 +0300

    Add simple index html file

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { join } from 'path'
 import { AppModule } from './app.module'
 import { createSwagger } from './swagger'
 import { createDocument as createTimetableDocument } from './timetable/src/swagger'
 import { createDocument as createTwitchBotDocument } from './twitch-bot/src/swagger'
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule)
+    const app = await NestFactory.create<NestExpressApplication>(AppModule)
+    app.useStaticAssets(join(__dirname, 'static'))
     const configService = app.get(ConfigService)
     createSwagger(app, createTwitchBotDocument(app), { path: 'v1/twitch-bot' })
     createSwagger(app, createTimetableDocument(app), { path: 'v2/timetable' })
