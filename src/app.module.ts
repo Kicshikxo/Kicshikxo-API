f872665d01db4ffe13c816ed0d9e24aaf6770417
Kicshikxo <supermegavalera9000@gmail.com>
2023-07-04 20:05:34 +0300

    Add memes bot submodule and update packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { RouterModule } from '@nestjs/core'
+import { BotModule as MemesBotModule } from './memes-bot/src/bot.module'
 import { TimetableModule } from './timetable/src/timetable.module'
-import { BotModule } from './twitch-bot/src/bot.module'
+import { BotModule as TwitchBotModule } from './twitch-bot/src/bot.module'
 
 @Module({
     imports: [
-        BotModule,
-        RouterModule.register([{ path: 'v1/twitch-bot', module: BotModule }]),
+        TwitchBotModule,
+        RouterModule.register([{ path: 'v1/twitch-bot', module: TwitchBotModule }]),
         TimetableModule,
         RouterModule.register([{ path: 'v2/timetable', module: TimetableModule }]),
+        MemesBotModule,
         ConfigModule.forRoot()
     ],
     controllers: [],
